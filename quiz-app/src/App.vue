19245037c74987852fa735169bdb6c3e4c02c01a
Anup Rai <anup.rai@ekbana.info>
2023-04-19 11:16:21 +0545

    feat: isolating the card into a separate component

--- quiz-app/src/App.vue
+++ quiz-app/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref, watch } from 'vue';
   import q from './data/quizes.json';
+  import Card from './components/Card.vue';
   const quizes = ref(q);
   const searchKeyword = ref('');
 
@@ -16,13 +17,7 @@
       <input v-model.trim="searchKeyword" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="">
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p> {{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card />
     </div>
   </div>
 </template>
@@ -51,35 +46,9 @@
     padding: 10px;
     border-radius: 5px;
   }
-
   .options-container {
     display: flex;
     flex-wrap: wrap;
     margin-top: 40px;
-  }
-
-  /* Card */
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0;
-  }
-
-  .card .card-text {
-    padding: 0 5px;
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
   }
 </style>
