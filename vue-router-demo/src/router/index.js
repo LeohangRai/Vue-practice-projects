77d62e383bcc1556f6c1ff32369d34be4570b69c
Anup Rai <anup.rai@ekbana.info>
2023-04-23 00:38:34 +0545

    feat: implement fallback route for undefined routes; 404 page

--- vue-router-demo/src/router/index.js
+++ vue-router-demo/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import CarView from "../views/CarView.vue";
 import ContactView from '../views/ContactView.vue';
+import NotFoundView from '../views/404View.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
         }
       ]
     },
+    {
+      path: "/:catchall(.*)*",
+      name: 'notFound',
+      component: NotFoundView
+    }
   ],
 });
 
