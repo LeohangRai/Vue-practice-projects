12641de5a683708dedf1daffefefa5a29ac0d42e
Anup Rai <anup.rai@ekbana.info>
2023-04-22 23:37:50 +0545

    feat: implement page navigation on a <button> instead of <RouterLink> with the help of the useRouter composable

--- vue-router-demo/src/views/CarView.vue
+++ vue-router-demo/src/views/CarView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import cars from "../data/cars.json";
-import { useRoute, RouterView } from "vue-router";
+import { useRoute, useRouter, RouterView } from "vue-router";
 const route = useRoute();
+const router = useRouter();
+
 const car = cars.find((c) => c.id === Number(route.params.id));
 </script>
 
@@ -11,6 +13,9 @@ const car = cars.find((c) => c.id === Number(route.params.id));
     <p>{{ car.name }}</p>
     <p>{{ car.year }}</p>
     <p>{{ car.price }}</p>
+    <button @click="router.push(`/cars/${car.id}/contact`)">
+      Click for Contact Info
+    </button>
     <RouterView />
   </div>
 </template>
