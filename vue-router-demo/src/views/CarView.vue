059227d18ce960ff9c9a3a6e82e78af4ad7b9266
Anup Rai <anup.rai@ekbana.info>
2023-04-22 17:01:14 +0545

    feat: add dynamic routes for individual car view

---
+++ vue-router-demo/src/views/CarView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import cars from "../data/cars.json";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const car = cars.find((c) => c.id === Number(route.params.id));
+</script>
+
+<template>
+  <div>
+    <h1>Car View</h1>
+    <p>{{ car.name }}</p>
+    <p>{{ car.year }}</p>
+    <p>{{ car.price }}</p>
+  </div>
+</template>
