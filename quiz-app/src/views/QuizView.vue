5151f2617f4d5c43e1e0b5979aefd04bf2c0944f
Anup Rai <anup.rai@ekbana.info>
2023-04-23 13:18:08 +0545

    feat: making use of 'computed' instead of 'watch'

--- quiz-app/src/views/QuizView.vue
+++ quiz-app/src/views/QuizView.vue
@@ -2,20 +2,15 @@
 import QuizHeader from '../components/QuizHeader.vue';
 import Question from '../components/Question.vue';
 import { useRoute } from 'vue-router';
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 import quizes from '../data/quizes.json';
 
 const route = useRoute();
 const currentQuizId = Number(route.params.id);
 const currentQuiz = quizes.find((q) => q.id === currentQuizId);
 const currentQuestionIndex = ref(0);
-const questionNumberStatus = ref(
-  `${currentQuestionIndex.value}/${currentQuiz.questions.length}`
-); // to show on the progress bar
 
-watch(currentQuestionIndex, () => {
-  questionNumberStatus.value = `${currentQuestionIndex.value}/${currentQuiz.questions.length}`;
-});
+const questionNumberStatus = computed(() => `${currentQuestionIndex.value}/${currentQuiz.questions.length}`);
 </script>
 
 <template>
