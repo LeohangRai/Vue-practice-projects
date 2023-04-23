f895b8b30850d3596e89d6ecc721333be9179f49
Anup Rai <anup.rai@ekbana.info>
2023-04-23 11:37:31 +0545

    feat: dynamically render quiz questions and options

--- quiz-app/src/components/Question.vue
+++ quiz-app/src/components/Question.vue
@@ -1,11 +1,18 @@
+<script setup>
+  import { defineProps } from 'vue';
+  const { question } = defineProps(['question']);
+</script>
+
 <template>
   <div class="question-container">
-    <h1 class="question">What is the height of Mt. Everest?</h1>
+    <h1 class="question">
+      {{ question.text }}
+    </h1>
   </div>
   <div class="options-container">
-    <div class="option">
-      <p class="option-label">A</p>
-      <p class="option-value">8858 meters</p>
+    <div v-for="option in question.options" :key="option.id" class="option">
+      <p class="option-label">{{ option.label }}</p>
+      <p class="option-value">{{ option.text }}</p>
     </div>
   </div>
 </template>
@@ -21,16 +28,16 @@
   }
 
   .option {
-    /* display: flex; */
-    margin-bottom: 20px;
     cursor: pointer;
     display: flex;
+    margin-bottom: 20px;
   }
 
   .option-label {
     background-color: bisque;
     width: 50px;
     height: 50px;
+    margin: 0;
     font-size: 30px;
     display: flex;
     align-items: center;
@@ -41,6 +48,7 @@
     background-color: rgb(244, 239, 239);
     width: 100%;
     height: 50px;
+    margin: 0;
     font-size: 30px;
     padding: 0 20px;
     display: flex;
--- quiz-app/src/views/QuizView.vue
+++ quiz-app/src/views/QuizView.vue
@@ -1,13 +1,22 @@
 <script setup>
 import QuizHeader from '../components/QuizHeader.vue'
 import Question from '../components/Question.vue';
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+import quizes from '../data/quizes.json';
+
+const route = useRoute();
+const currentQuizId = Number(route.params.id);
+const currentQuiz = quizes.find(q => q.id === currentQuizId);
+const currentQuestionIndex = ref(0)
+
 </script>
 
 <template>
   <div>
     <QuizHeader />
     <div>
-      <Question />
+      <Question :question="currentQuiz.questions[currentQuestionIndex]" />
     </div>
   </div>
 </template>
