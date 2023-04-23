73279cf182ff4a46c7590e380464398c7e36cff4
Anup Rai <anup.rai@ekbana.info>
2023-04-23 10:52:57 +0545

    feat: refactor the QuizView into separate components

---
+++ quiz-app/src/components/Question.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="question-container">
+    <h1 class="question">What is the height of Mt. Everest?</h1>
+  </div>
+  <div class="options-container">
+    <div class="option">
+      <p class="option-label">A</p>
+      <p class="option-value">8858 meters</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .question-container {
+    margin-top: 20px;
+  }
+
+  .question {
+    font-size: 40px;
+    margin-bottom: 20px;
+  }
+
+  .option {
+    /* display: flex; */
+    margin-bottom: 20px;
+    cursor: pointer;
+    display: flex;
+  }
+
+  .option-label {
+    background-color: bisque;
+    width: 50px;
+    height: 50px;
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .option-value {
+    background-color: rgb(244, 239, 239);
+    width: 100%;
+    height: 50px;
+    font-size: 30px;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+  }
+</style>
--- quiz-app/src/views/QuizView.vue
+++ quiz-app/src/views/QuizView.vue
@@ -1,79 +1,14 @@
+<script setup>
+import QuizHeader from '../components/QuizHeader.vue'
+import Question from '../components/Question.vue';
+</script>
+
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuizHeader />
     <div>
-      <div class="question-container">
-        <h1 class="question">What is the height of Mt. Everest?</h1>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <p class="option-value">8858 meters</p>
-        </div>
-      </div>
+      <Question />
     </div>
   </div>
 </template>
 
-<style scoped>
-  header {
-    margin-top: 20px;
-  }
-
-  header h4 {
-    font-size: 30px;
-  }
-
-  .bar {
-    width: 300px;
-    height: 50px;
-    border: 3px solid bisque;
-  }
-
-  .completion {
-    height: 100%;
-    width: 0%;
-    background-color: bisque;
-  }
-
-  .question-container {
-    margin-top: 20px;
-  }
-
-  .question {
-    font-size: 40px;
-    margin-bottom: 20px;
-  }
-
-  .option {
-    /* display: flex; */
-    margin-bottom: 20px;
-    cursor: pointer;
-    display: flex;
-  }
-
-  .option-label {
-    background-color: bisque;
-    width: 50px;
-    height: 50px;
-    font-size: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .option-value {
-    background-color: rgb(244, 239, 239);
-    width: 100%;
-    height: 50px;
-    font-size: 30px;
-    padding: 0 20px;
-    display: flex;
-    align-items: center;
-  }
-</style>
