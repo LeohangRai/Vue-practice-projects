d5f500eb5dde39f1546e094eac25ab230cde9007
Anup Rai <anup.rai@ekbana.info>
2023-04-25 23:26:22 +0545

    feat: show results after all the quesions have been answered

---
+++ quiz-app/src/components/Result.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+const { totalNumOfQuestions, numOfCorrectAnswers } = defineProps([
+  'totalNumOfQuestions',
+  'numOfCorrectAnswers'
+]);
+</script>
+
+<template>
+  <div class="results">
+    <p>Your results...</p>
+    <h1>{{ `${numOfCorrectAnswers}/${totalNumOfQuestions}` }}</h1>
+    <RouterLink to="/">Back to Home</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+  .results {
+    text-align: center;
+    padding: 100px 0;
+  }
+
+  p {
+    font-size: 25px;
+  }
+
+  h1 {
+    font-size: 80px;
+  }
+</style>
--- quiz-app/src/views/QuizView.vue
+++ quiz-app/src/views/QuizView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import QuizHeader from '../components/QuizHeader.vue';
 import Question from '../components/Question.vue';
+import Result from '../components/Result.vue';
 import { useRoute } from 'vue-router';
 import { ref, computed } from 'vue';
 import quizes from '../data/quizes.json';
@@ -10,6 +11,7 @@ const currentQuizId = Number(route.params.id);
 const currentQuiz = quizes.find((q) => q.id === currentQuizId);
 const currentQuestionIndex = ref(0);
 const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
 
 /* To show Question 1/3 --> Question 2/3 and so on */
 const questionNumberStatus = computed(
@@ -24,6 +26,9 @@ function optionSelectionHandler(isCorrect) {
   if (isCorrect) {
     numberOfCorrectAnswers.value++;
   }
+  if (currentQuestionIndex.value === currentQuiz.questions.length - 1) {
+    showResults.value = true;
+  }
   currentQuestionIndex.value++;
 }
 </script>
@@ -35,11 +40,16 @@ function optionSelectionHandler(isCorrect) {
       :barPercent="barPercent"
     />
     <div>
+      <Result
+        v-if="showResults"
+        :totalNumOfQuestions="currentQuiz.questions.length"
+        :numOfCorrectAnswers="numberOfCorrectAnswers"
+      />
       <Question
+        v-else
         :question="currentQuiz.questions[currentQuestionIndex]"
         @optionSelected="optionSelectionHandler"
       />
-      {{ numberOfCorrectAnswers }}
     </div>
   </div>
 </template>
