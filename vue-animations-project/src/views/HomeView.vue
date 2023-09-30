b61db2d7dea315789a62fc62e8210b1fea3632e0
Anup Rai <anup.rai@ekbana.info>
2023-09-30 13:30:53 +0545

    feat: animations-project wip

--- vue-animations-project/src/views/HomeView.vue
+++ vue-animations-project/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { ref } from 'vue';
+const showText = ref(true);
 </script>
 
 <template>
   <main>
+    <h1 v-if="showText">Hello</h1>
+    <button @click="showText = !showText">Toggle</button>
   </main>
 </template>
